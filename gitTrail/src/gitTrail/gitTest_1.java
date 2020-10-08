aa4417785b2e7a2a4f6046182d85b8757ad793a5
GanaRko <GanaRko@gsphub>
2020-10-09 00:24:45 +0530

    Added a Class gitTest

---
+++ gitTrail/src/gitTrail/gitTest_1.java
@@ -0,0 +1,18 @@
+package gitTrail;
+
+public class gitTest_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr = {12,-12,67,89,40,-45,46,45,89,97};
+
+        //quicksorting(arr,0,arr.length);
+
+        for(int i=0;i<arr.length;i++)
+                {
+        	       System.out.print(arr[i]+" ");
+                }
+        System.out.println("Hello Doctor!!!");
+	}
+
+}
